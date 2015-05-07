f6a73fdc9f93abf34ef7878c6bd8afa41cfd5af5
SaraSaif <seifi.sarah@gmail.com>
2015-05-07 10:56:50 +0200

    Update step3.js

--- app/password/step3.js
+++ app/password/step3.js
@@ -15,6 +15,20 @@ define(['jquery', 'knockout', 'plugins/router', 'api/wordList', './model'], func
     self.remaningAttempts = ko.observable();
     self.showHelp = ko.observable(false);
 
+    var modifiedWordList = [];
+    ko.utils.arrayForEach(wordList, function(item){
+        if(item.indexOf('ä') !== -1) {
+           item = replaceSwedishChar(item, "ä", "a");
+        }
+        if(item.indexOf('å') !== -1) {
+           item = replaceSwedishChar(item, "å", "a");
+        }
+        if(item.indexOf('ö') !== -1) {
+            item = replaceSwedishChar(item, "ö", "o");
+        }
+        modifiedWordList.push(item);
+    });
+
     init();
     var firebaseRef = new Firebase("https://blistering-inferno-350.firebaseIO.com/");
 
@@ -27,10 +41,10 @@ define(['jquery', 'knockout', 'plugins/router', 'api/wordList', './model'], func
       for (var j=0; j<5; j++){
         for(var i=0; i<3 ; i++){
             generatedWord[i] = Math.floor(Math.random()*475)+1;
-            wordFromDict[i] = wordList[generatedWord[i]];         
+            wordFromDict[i] = modifiedWordList[generatedWord[i]];         
         }
         if(self.symbol1() && self.symbol2()){             
-        phrase[j] = capitalizeFirstLetter(wordFromDict[0]) + self.symbol1()  + wordFromDict[1] + self.symbol2() + wordFromDict[2];
+        phrase[j] =wordFromDict[0] + self.symbol1()  + wordFromDict[1] + self.symbol2() + wordFromDict[2];
         self.passPhrases.push(phrase[j]);
         }    
       }
@@ -69,10 +83,16 @@ define(['jquery', 'knockout', 'plugins/router', 'api/wordList', './model'], func
     this.back = function () {
        router.navigate('#step2/');
     }
-    
-    function capitalizeFirstLetter(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
+
+    function replaceSwedishChar (word, swedChar, engChar){
+      var swedishCharIndex = word.indexOf(swedChar);
+      var lastIndex = word.length;
+      return word = word.slice(0, swedishCharIndex) +  engChar + word.slice(swedishCharIndex+1, lastIndex);  
     }
+
+    // function capitalizeFirstLetter(string) {
+    // return string.charAt(0).toUpperCase() + string.slice(1);
+    // }
 }
   Ctor.prototype.activate = function () {
     $("#progressbar li").eq(0).addClass('active');
