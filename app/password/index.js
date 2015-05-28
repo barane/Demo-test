9847cbbbf61940f250e334f6f0708c9e4440e411
SaraSaif <seifi.sarah@gmail.com>
2015-05-28 08:40:39 +0200

    Update index.js

--- app/password/index.js
+++ app/password/index.js
@@ -1,18 +1,21 @@
 define(['jquery', 'knockout', 'plugins/router','./step2', './model'], function ($, ko, router, step2, model ){
 
  var Ctor = function () {
+ 	var self = this;
     self.showHelp = ko.observable(false);
     self.userName = ko.observable();
+    self.selectedLang = ko.observable('Swedish');
 
- this.help= function(){
- 	self.showHelp(!self.showHelp())
- }   
-  
- this.sendSMS = function(){
-  router.navigate('#step2/');
-  model.userName = self.userName();
- };
-
+	this.help= function(){
+	 	self.showHelp(!self.showHelp())
+	} 
+	  
+	this.next = function(){
+	  router.navigate('#step2/');
+	  model.userName = self.userName();
+	  model.selectedLang = self.selectedLang();
+	};
+ 
 }
 
 Ctor.prototype.activate = function () {
