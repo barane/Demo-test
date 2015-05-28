471ca705f1886e55f4d43bcba4c46a054a2d85db
SaraSaif <seifi.sarah@gmail.com>
2015-05-28 08:39:08 +0200

    Update confirme.js

--- app/password/confirme.js
+++ app/password/confirme.js
@@ -1,6 +1,18 @@
-define(['jquery', 'knockout', 'plugins/router','./step2'], function ($, ko, router, step2 ){
+define(['jquery', 'knockout', 'plugins/router','./step2', "./model"], function ($, ko, router, step2, model ){
 
  var Ctor = function () {
+ 	var self = this;
+ 	self.passWord = model.passWord.complete;
+ 	self.enteredValue = ko.observable();
+ 	this.submit = function(){
+ 		if(!self.enteredValue()){
+ 			window.alert("Please enter your password first!");
+ 		} else if(self.enteredValue() == self.passWord){
+ 			window.alert("CORRECT!");
+ 		} else {
+ 			window.alert("WRONG, your password is: "+ self.passWord)
+ 		}
+ 	}
  
 }
 
