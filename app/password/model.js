9735725f33d60ae8166452e461e3ada230e94693
SaraSaif <seifi.sarah@gmail.com>
2015-05-28 08:41:14 +0200

    Update model.js

--- app/password/model.js
+++ app/password/model.js
@@ -1,11 +1,7 @@
 define(['knockout'], function(ko){
-
 	var model = {};
-
-
-	   model.userName = ko.observable();		
-	  
-
-
+	   model.userName = ko.observable();
+	   model.passWord = ko.observable();
+	   model.selectedLang = ko.observable();		  
 	return model;
 })
